5aa6eb1ebc917fce0df4b45c5fb5a2ce183ffe1f
Nikita Garmoza <garmozanikita@gmail.com>
2024-05-14 15:16:34 +0300

    fix styles

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -2,15 +2,15 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.Toolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val imageView = findViewById<ImageView>(R.id.imageViewBack)
-        imageView.setOnClickListener {
+        val toolbarSettings = findViewById<Toolbar>(R.id.toolbarSettings)
+        toolbarSettings.setOnClickListener {
             finish()
         }
     }
