017e26406a7fa19a3296581dd11b55aeb6c2254a
Nikita Garmoza <garmozanikita@gmail.com>
2024-06-29 22:03:47 +0300

    adds SearchActivity logic

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,7 +1,14 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toolbar
 
 class SearchActivity : AppCompatActivity() {
@@ -13,5 +20,30 @@ class SearchActivity : AppCompatActivity() {
         toolbarSettings.setOnClickListener {
             finish()
         }
+
+        val editTextSearch = findViewById<EditText>(R.id.editTextSearch)
+        val imageViewClear = findViewById<ImageView>(R.id.imageViewClear)
+
+        imageViewClear.setOnClickListener {
+            editTextSearch.setText("")
+            editTextSearch.clearFocus()
+            val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            manager.hideSoftInputFromWindow(editTextSearch.windowToken, 0)
+        }
+
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                imageViewClear.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        editTextSearch.addTextChangedListener(searchTextWatcher)
     }
 }
