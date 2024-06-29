e77142ad131e5f33dd1eb845f4bab3f9490e7460
Nikita Garmoza <garmozanikita@gmail.com>
2024-06-29 21:28:25 +0300

    adds styles for search input

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -2,10 +2,16 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val toolbarSettings = findViewById<Toolbar>(R.id.toolbarSearch)
+        toolbarSettings.setOnClickListener {
+            finish()
+        }
     }
 }
