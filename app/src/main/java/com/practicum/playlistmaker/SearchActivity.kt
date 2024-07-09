6189ba767493ec02dadb37ce77de66c1165cccea
garmoza <59695286+garmoza@users.noreply.github.com>
2024-07-09 21:55:09 +0300

    Sprint 9 task (#2)

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,69 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toolbar
 
 class SearchActivity : AppCompatActivity() {
+
+    private var searchedValue = DEFAULT_SEARCHED_VALUE
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val toolbarSettings = findViewById<Toolbar>(R.id.toolbarSearch)
+        toolbarSettings.setOnClickListener {
+            finish()
+        }
+
+        val editTextSearch = findViewById<EditText>(R.id.editTextSearch)
+        val imageViewClear = findViewById<ImageView>(R.id.imageViewClear)
+
+        imageViewClear.setOnClickListener {
+            editTextSearch.setText("")
+            editTextSearch.clearFocus()
+            val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            manager.hideSoftInputFromWindow(editTextSearch.windowToken, 0)
+        }
+
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchedValue = s.toString()
+                imageViewClear.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        editTextSearch.addTextChangedListener(searchTextWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCHED_VALUE_KEY, searchedValue)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchedValue = savedInstanceState.getString(SEARCHED_VALUE_KEY, DEFAULT_SEARCHED_VALUE)
+        findViewById<EditText>(R.id.editTextSearch).setText(searchedValue)
+    }
+
+    companion object {
+        const val SEARCHED_VALUE_KEY = "SEARCH_VALUE"
+        const val DEFAULT_SEARCHED_VALUE = ""
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,10 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toolbar
 
 class SettingsActivity : AppCompatActivity() {
@@ -12,6 +15,38 @@ class SettingsActivity : AppCompatActivity() {
         val toolbarSettings = findViewById<Toolbar>(R.id.toolbarSettings)
         toolbarSettings.setOnClickListener {
             finish()
+        }
+
+        val textViewShareApp = findViewById<TextView>(R.id.textViewShareApp)
+        textViewShareApp.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(
+                Intent.EXTRA_TEXT,
+                getString(R.string.ya_practicum_android_course_url)
+            )
+            startActivity(shareIntent)
+        }
+
+        val textViewSupport = findViewById<TextView>(R.id.textViewSupport)
+        textViewSupport.setOnClickListener {
+            val sendToAddresses = arrayOf(
+                getString(R.string.developer_email)
+            )
+
+            val sendMailIntent = Intent(Intent.ACTION_SENDTO)
+            sendMailIntent.setData(Uri.parse("mailto:"))
+            sendMailIntent.putExtra(Intent.EXTRA_EMAIL, sendToAddresses)
+            sendMailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_email_subject))
+            sendMailIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.support_email_body))
+            startActivity(sendMailIntent)
+        }
+
+        val textViewLicenseAgreement = findViewById<TextView>(R.id.textViewLicenseAgreement)
+        textViewLicenseAgreement.setOnClickListener {
+            val uri = Uri.parse(getString(R.string.ya_practicum_license_agreement_url))
+            val browseIntent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(browseIntent)
         }
     }
 }
