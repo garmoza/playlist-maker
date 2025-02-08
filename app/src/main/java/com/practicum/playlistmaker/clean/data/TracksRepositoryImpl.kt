90c98ba2cff1e93c2f6b2d4a76408f85482f9d7e
Nikita Garmoza <garmozanikita@gmail.com>
2025-02-08 20:35:53 +0300

    add mapper

--- app/src/main/java/com/practicum/playlistmaker/clean/data/TracksRepositoryImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/clean/data/TracksRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.practicum.playlistmaker.clean.data
 
 import com.practicum.playlistmaker.clean.data.dto.ITunseSearchRequest
 import com.practicum.playlistmaker.clean.data.dto.ITunseTracksResponse
+import com.practicum.playlistmaker.clean.data.mapper.TrackMapper
 import com.practicum.playlistmaker.clean.domain.exceptions.BadResponseException
 import com.practicum.playlistmaker.clean.domain.api.TracksRepository
 import com.practicum.playlistmaker.clean.domain.models.Track
@@ -13,20 +14,7 @@ class TracksRepositoryImpl(
     override fun searchTracks(expression: String): List<Track> {
         val response = networkClient.doRequest(ITunseSearchRequest(expression))
         return if (response.resultCode == 200) {
-            (response as ITunseTracksResponse).results.map {
-                Track(
-                    trackId = it.trackId,
-                    trackName = it.trackName,
-                    artistName = it.artistName,
-                    trackTimeMillis = it.trackTimeMillis,
-                    artworkUrl100 = it.artworkUrl100,
-                    collectionName = it.collectionName,
-                    releaseDate = it.releaseDate,
-                    primaryGenreName = it.primaryGenreName,
-                    country = it.country,
-                    previewUrl = it.previewUrl
-                )
-            }
+            (response as ITunseTracksResponse).results.map(TrackMapper::map)
         } else {
             throw BadResponseException()
         }
