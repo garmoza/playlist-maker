b00230f138e959eec13291b9301cdf3b9c9256a3
Nikita Garmoza <garmozanikita@gmail.com>
2025-05-04 21:33:36 +0300

    move Retrofit dependence to DataModule

--- app/src/main/java/com/practicum/playlistmaker/di/DataModule.kt
+++ app/src/main/java/com/practicum/playlistmaker/di/DataModule.kt
@@ -9,11 +9,16 @@ import com.practicum.playlistmaker.common.data.network.NetworkClient
 import com.practicum.playlistmaker.common.data.preferences.PLAYLIST_MAKER_PREFERENCES
 import com.practicum.playlistmaker.palyer.data.PlayerImpl
 import com.practicum.playlistmaker.palyer.domain.Player
+import com.practicum.playlistmaker.search.data.network.ITunseApiService
 import com.practicum.playlistmaker.search.data.network.ITunseRetrofitNetworkClient
 import com.practicum.playlistmaker.sharing.data.ExternalNavigatorImpl
 import com.practicum.playlistmaker.sharing.domain.ExternalNavigator
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val ITUNSE_BASE_URL = "https://itunes.apple.com"
 
 val dataModule = module {
     single<SharedPreferences> {
@@ -25,11 +30,19 @@ val dataModule = module {
         androidContext().resources
     }
 
+    single<ITunseApiService> {
+        Retrofit.Builder()
+            .baseUrl(ITUNSE_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ITunseApiService::class.java)
+    }
+
     single<NetworkClient> {
         val connectivityManager = androidContext().getSystemService(
             Context.CONNECTIVITY_SERVICE
         ) as ConnectivityManager
-        ITunseRetrofitNetworkClient(connectivityManager)
+        ITunseRetrofitNetworkClient(connectivityManager, get())
     }
 
     factory<Player> {
