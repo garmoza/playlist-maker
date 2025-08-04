65d937428826797a863478cb66252961755f7820
Nikita Garmoza <garmozanikita@gmail.com>
2025-08-04 18:45:00 +0300

    add BottomNavigationView

--- app/src/main/java/com/practicum/playlistmaker/main/ui/activity/HostActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/main/ui/activity/HostActivity.kt
@@ -1,7 +1,11 @@
 package com.practicum.playlistmaker.main.ui.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.ActivityHostBinding
 
 class HostActivity : AppCompatActivity() {
@@ -12,5 +16,22 @@ class HostActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHostBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // use supportFragmentManager to maintain backward compatibility and avoid bugs
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.searchFragment, R.id.libraryFragment, R.id.settingsFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                else -> {
+                    binding.bottomNavigationView.visibility = View.GONE
+                }
+            }
+        }
     }
 }
