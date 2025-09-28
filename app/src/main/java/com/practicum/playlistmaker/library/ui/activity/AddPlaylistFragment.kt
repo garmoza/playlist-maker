ba403aa932131d4beb39a5d63059ca3cd86b6cc2
Nikita Garmoza <garmozanikita@gmail.com>
2025-09-28 16:45:03 +0300

    use PickVisualMedia

--- app/src/main/java/com/practicum/playlistmaker/library/ui/activity/AddPlaylistFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/library/ui/activity/AddPlaylistFragment.kt
@@ -1,11 +1,18 @@
 package com.practicum.playlistmaker.library.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.practicum.playlistmaker.common.ui.dpToPx
 import com.practicum.playlistmaker.databinding.FragmentAddPlaylistBinding
 
 class AddPlaylistFragment : Fragment() {
@@ -27,6 +34,21 @@ class AddPlaylistFragment : Fragment() {
 
         binding.toolbar.setOnClickListener {
             findNavController().navigateUp()
+        }
+
+        val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                Glide.with(this)
+                    .load(uri)
+                    .transform(CenterCrop(), RoundedCorners(dpToPx(8F, requireContext())))
+                    .into(binding.playlistLabel)
+            } else {
+                Log.i("AddPlaylistFragment", "No media selected")
+            }
+        }
+
+        binding.playlistLabel.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
     }
 
